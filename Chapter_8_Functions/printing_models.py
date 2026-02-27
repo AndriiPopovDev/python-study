1fd398a4f06b7793822c944c1e5bf352684d8e14
AndriiPopovDev <andriipopov0110@gmail.com>
2026-02-27 16:48:07 +0200

    mod printing_models.py

--- Chapter_8_Functions/printing_models.py
+++ Chapter_8_Functions/printing_models.py
@@ -1,3 +1,4 @@
+"""
 unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedrone']
 completed_models = []
 
@@ -10,3 +11,34 @@ while unprinted_designs:
 print(f"\nThe following models have been printed:")
 for completed_model in completed_models:
     print(f"\t{completed_model}")
+"""
+
+
+def print_models(unprinted_designs, completed_model):
+    """
+    Simulate printing each design, until none are left.
+    Move each design to complete_models after printing.
+    """
+
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(complete_models):
+    """
+    Show all the models that were printed
+    """
+
+    print("\nThe following models have been printed:")
+    for model in completed_models:
+        print(f"\t{model}")
+
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
