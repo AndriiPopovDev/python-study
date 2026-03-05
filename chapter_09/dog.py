ab4a6554a1657a39e529769b77c9f122a7a27a32
AndriiPopovDev <andriipopov0110@gmail.com>
2026-03-05 16:02:26 +0200

    minor

--- chapter_09/dog.py
+++ chapter_09/dog.py
@@ -9,3 +9,21 @@ class Dog():
     def sit(self):
         """Simulate a dog sitting in response to a command"""
         print(f"{self.name.title()} is now sitting")
+
+    def roll(self):
+        """Simulate rolling over in response to a command"""
+        print(f"{self.name.title()} rolled over!")
+
+        
+my_dog = Dog('dem', 8)
+your_dog = Dog('lucy', 3)
+
+print(f"My dog's name is {my_dog.name.title()}")
+print(f"My dog is {my_dog.age} years old")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name.title()}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
+
+
