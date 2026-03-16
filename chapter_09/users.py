6d71c78f6fd3c2d6c6948361d20b74f5e9a6b2e8
AndriiPopovDev <andriipopov0110@gmail.com>
2026-03-16 12:25:43 +0200

    refactor users.py

--- chapter_09/users.py
+++ chapter_09/users.py
@@ -1,25 +1,30 @@
-class Users():
-    def __init__(self, first_name, last_name, age=0, city=0, proffesion=0):
+class User():
+    
+    def __init__(self,
+                first_name,
+                last_name,
+                password='',
+                id='',
+                mail='',):
+        
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
-        self.city = city
-        self.proffesion = proffesion 
+        self.password = password
+        self.id = id
+        self.mail = mail
 
     def describe_user(self):
-        print(f"{self.first_name.title()} {self.last_name.title()} "
-                f"is {self.age}, work {self.proffesion}, live in {self.city.title()}")
+        print(f"Name: {self.first_name.title()} {self.last_name.title()}")
     
     def greet_user(self):
         print(f"Greetings, {self.first_name.title()} {self.last_name.title()}!")
         
 
-andrii = Users('andrii', 'popov', 24, 'kyiv', 'coder')
-alina = Users('alina', 'chernish', 24, 'kyiv', 'analyst')
+users = [User('alina', 'chernish'),
+         User('andrii', 'popov'),
+         User('vlad', 'romanov')]
 
-andrii.describe_user()
-andrii.greet_user()
-print("\n")
-
-alina.describe_user()
-alina.greet_user()
+for user in users:
+    user.describe_user()
+    user.greet_user()
+    print('\n')
