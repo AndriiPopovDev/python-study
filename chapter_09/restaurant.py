acab818bd8b56d4560f74caa6b06ecc4867560be
AndriiPopovDev <andriipopov0110@gmail.com>
2026-03-16 18:19:34 +0200

    refactor restaurant.py to encapsulate number served logic and streamline restaurant instances

--- chapter_09/restaurant.py
+++ chapter_09/restaurant.py
@@ -2,25 +2,37 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"The {self.restaurant_name.title()} has {self.cuisine_type}.")
 
     def open_restaurant(self):
         print(f"The {self.restaurant_name.title()} is open")
+        
+    def set_number_served(self, number):
+        self.number_served = number
+    
+    def increment_number_served(self, number):
+        self.number_served += number
 
 
-riso = Restaurant("riso", "sushi")
-mcdonalds = Restaurant("mc donalds", "fast food")
-idealist = Restaurant("idealist", "coffee")
+restaurants = [
+                Restaurant("the mean queen", "pizza"),
+                Restaurant("minani musashi", "sushi"),
+                Restaurant("riso", "sushi"),
+                Restaurant("mc donalds", "fast food"),
+                Restaurant("idealist", "coffee"),
+               ]
 
-riso.describe_restaurant()
-riso.open_restaurant()
+print(f"\nNumber served: {restaurants[0].number_served}")
+
+restaurants[0].set_number_served(100)
+print(f"Number served: {restaurants[0].number_served}")
+
+restaurants[0].increment_number_served(2)
+print(f"Number served: {restaurants[0].number_served}")
 
 print("\n")
-mcdonalds.describe_restaurant()
-mcdonalds.open_restaurant()
-
-print("\n")
-idealist.describe_restaurant()
-idealist.open_restaurant()
+for r in restaurants:
+    print(f"Restaurant name: {r.restaurant_name.title()}, Number served: {r.number_served}")
