e11d703be84223f66f0de709e4b8592242187f5d
AndriiPopovDev <andriipopov0110@gmail.com>
2026-03-16 16:01:34 +0200

    refactoring, addin solution web

--- chapter_09/users.py
+++ chapter_09/users.py
@@ -3,26 +3,29 @@ class User():
     def __init__(self,
                 first_name,
                 last_name,
-                password='',
+                username,
                 id='',
                 mail='',):
         
-        self.first_name = first_name
-        self.last_name = last_name
-        self.password = password
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.username = username
         self.id = id
         self.mail = mail
 
     def describe_user(self):
-        print(f"Name: {self.first_name.title()} {self.last_name.title()}")
+        print(f"\nName: {self.first_name} {self.last_name}")
+        print(f"Username: {self.username}")
+        print(f"ID: {self.id}")
+        print(f"Mail: {self.mail}")
     
     def greet_user(self):
-        print(f"Greetings, {self.first_name.title()} {self.last_name.title()}!")
+        print(f"\nWerlcome back, {self.first_name} {self.last_name}!")
         
 
-users = [User('alina', 'chernish'),
-         User('andrii', 'popov'),
-         User('vlad', 'romanov')]
+users = [User('alina', 'chernish', 'alicher', '150', 'alina@gmail.com'),
+         User('andrii', 'popov', 'andriP', '151', 'andrii@gmail.com'),
+         User('vlad', 'romanov', 'romashka', '152', 'vlad@gmail.com')]
 
 for user in users:
     user.describe_user()
