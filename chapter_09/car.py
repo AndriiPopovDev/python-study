ac89aab86c51a250f773d4e1a20289753309dd8e
AndriiPopovDev <andriipopov0110@gmail.com>
2026-03-19 13:34:59 +0200

    add ElectricCar class with battery description and create my_electric_car.py

--- chapter_09/car.py
+++ chapter_09/car.py
@@ -32,10 +32,29 @@ class Car():
         self.odometer_reading += miles
 
 
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles"""
+    def __init__(self, make, model, year, battery_size=0):
+        """
+        Initialize attributes of the parent class
+        Tnan initialize specific attributes for the electric car
+        """
+        super().__init__(make, model, year)
+        self.battery_size = battery_size
 
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print(f"This car has a {self.battery_size} kWh battery")
 
 my_new_car = Car('audi', 'a4', 2016)
+tesla = ElectricCar('tesla', 'model s', 2016, 85)
+
 print(my_new_car.get_descriptive_name()) 
+print(tesla.get_descriptive_name())
+tesla.describe_battery()
+
+
+"""
 my_new_car.read_odometr()
 
 my_new_car.update_odometr(432)
@@ -44,3 +63,4 @@ my_new_car.update_odometr(300)
 
 my_new_car.increment_odometer(100)
 my_new_car.read_odometr()
+"""
