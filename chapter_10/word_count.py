602fa8c5c09352735d635864bc0d91d8f5bc2eea
Artem <kavunenkoartem@gmail.com>
2026-04-04 10:49:48 +0300

    new files

---
+++ chapter_10/word_count.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+def count_words(path):
+    # count the approximate number of words in the file: 
+    path = Path(path)
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"{path} doesn't exist")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {path} has about {num_words} words")
+
+
+filename = ['siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for file in filename:
+    path = Path(file)
+    count_words(path)
+
